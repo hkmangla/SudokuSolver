16e10f40db82b088de15bc6c00bc653cd097e99f
Hemant Mangla <hemantmangla78@gmail.com>
2017-09-09 21:46:54 +0000

    Added Input format

--- sudoku.py
+++ sudoku.py
@@ -2,6 +2,7 @@ from game import GameState
 from Agent import Agent
 print "Enter Size(N) of game N*N i.e. N: "
 size = input()
+print "Enter sudoku space separated elements and '0' for empty element: "
 gameList = []
 for i in range(size):
     l = [int(x) for x in raw_input().split(' ')]
